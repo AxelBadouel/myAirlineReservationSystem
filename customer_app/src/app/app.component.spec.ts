6c50301388de4a4077f47a89dfda10fb9b83b285
AxelSIMO <s261953@studenti.polito.it>
2024-11-19 15:59:44 +0100

    Modifying error in name from customer_ap to customer_app

--- customer_app/src/app/app.component.spec.ts
+++ customer_app/src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'customer_ap' title`, () => {
+  it(`should have the 'customer_app' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('customer_ap');
+    expect(app.title).toEqual('customer_appp');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, customer_ap');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, customer_app');
   });
 });
--- customer_app/src/app/app.component.ts
+++ customer_app/src/app/app.component.ts
@@ -9,5 +9,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'customer_ap';
+  title = 'customer_app';
 }
