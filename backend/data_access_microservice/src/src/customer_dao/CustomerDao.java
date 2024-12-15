da5d0655ab691d59c6122f9e03891c7512b17560
AxelSIMO <axel.simokengne@gmail.com>
2024-12-15 23:27:30 +0100

    Begining implementing CRUD

--- backend/data_access_microservice/src/src/customer_dao/CustomerDao.java
+++ backend/data_access_microservice/src/src/customer_dao/CustomerDao.java
@@ -1,4 +1,24 @@
 package customer_dao;
 
-public record CustomerDao() {
+import java.io.Serializable;
+import java.time.LocalDateTime;
+import java.util.Objects;
+
+public record CustomerDao(Integer customer_id,
+                          String customer_name,
+                          String city_departure,
+                          String city_destination,
+                          LocalDateTime datetime_of_flight,
+                          Integer booking_status,
+                          Double ticket_cost,
+                          String email) implements Serializable {
+
+    public CustomerDao {
+        Objects.requireNonNull(customer_id);
+        Objects.requireNonNull(customer_name);
+
+        if(city_departure.equals(city_destination)) {
+            throw new RuntimeException("City of departure and arrival cannot be the same");
+        }
+    }
 }
---
+++ backend/data_access_microservice/src/src/customer_dao/CustomerQueries.java
@@ -0,0 +1,10 @@
+package customer_dao;
+
+class CustomerQueries {
+    String CREATE = "";
+    String GET_WITH_ID = "SELECT * FROM customer_data WHERE customer_id=?";
+    String GET_ALL = "SELECT * FROM customer_data";
+    String UPDATE = "";
+    String DELETE_WITH_ID = "";
+    String DELETE_LIST = "";
+}
