fdf935015f612678b0ffa1c2d043a9a98292701e
AxelSIMO <axel.simokengne@gmail.com>
2024-12-16 23:58:15 +0100

    Insert function created for customer table

--- backend/data_access_microservice/src/src/customer_dao/CustomerDataManagement.java
+++ backend/data_access_microservice/src/src/customer_dao/CustomerDataManagement.java
@@ -1,4 +1,24 @@
 package customer_dao;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 public class CustomerDataManagement {
+    private final Connection connection;
+
+    public CustomerDataManagement(Connection connection) {
+        this.connection = connection;
+    }
+
+    public int insertNewCustomer(CustomerDao customerDao) throws SQLException {
+        PreparedStatement preparedStatement = connection.prepareStatement(CustomerQueries.INSERT_CUSTOMER);
+
+        int rowsInserted = preparedStatement.executeUpdate();
+        if(rowsInserted > 0) {
+            System.out.println("Customer inserted successfully");
+        }
+
+        return rowsInserted;
+    }
 }
--- backend/data_access_microservice/src/src/customer_dao/CustomerQueries.java
+++ backend/data_access_microservice/src/src/customer_dao/CustomerQueries.java
@@ -1,10 +1,9 @@
 package customer_dao;
 
 class CustomerQueries {
-    String CREATE = "";
-    String GET_WITH_ID = "SELECT * FROM customer_data WHERE customer_id=?";
-    String GET_ALL = "SELECT * FROM customer_data";
-    String UPDATE = "";
-    String DELETE_WITH_ID = "";
-    String DELETE_LIST = "";
+    public static final String INSERT_CUSTOMER = "INSERT INTO customer_data (customer_name, city_departure, city_destination, datetime_of_flight, booking_status, ticket_cost, email) VALUES (?, ?, ?, ?, ?, ?, ?)";
+    public static final String GET_WITH_ID = "SELECT * FROM customer_data WHERE customer_id=?";
+    public static final String GET_ALL = "SELECT * FROM customer_data";
+    public static final String UPDATE = "UPDATE customer_data SET customer_name=?, city_departure=?, city_destination=?, datetime_of_flight=?, booking_status=?, ticket_cost=?, email=? WHERE username=?";
+    public static final String DELETE_WITH_ID = "DELETE FROM customer_data WHERE customer_id=?";
 }
